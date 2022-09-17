86074ffccb86405a6255346382d5db2a2d8e7196
Cole Barelli <ColeBarelli@github.com>
2022-09-17 09:35:07 -0400

    added code for porfolio
    added code to portfolio

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+btn = document.getElementById("btn").addEventListener("click", myFunction)
+social = document.getElementById("socialLink").addEventListener("click", myFunction2)
+social = document.getElementById("socialLink2").addEventListener("click", myFunction2)
+social = document.getElementById("socialLink3").addEventListener("click", myFunction2)
+
+
+function myFunction() {
+  alert("clicked")
+}
+
+function myFunction2() {
+  alert("Sorry the person you are trying to talk to right now cannot reach the phone.")
+}
